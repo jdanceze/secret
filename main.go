bf4e1434be22800995cddb4e7c591a9a2ffd372d
Pattarakrit Rattanukul <jdanceze@Pattarakrits-MacBook-Pro.local>
2022-07-16 00:03:09 +0700

    add reverse function

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"fmt"
 )
 
+func reverse(str string) (result string) {
+	for _, char := range str {
+		result = string(char) + result
+	}
+	return
+}
+
 func main() {
 	var whatIsIt string
 
 	secret := "aWFuZ25vVzpOQU06RU5JTDp0YTpzdTpuaW9K"
 
 	sd, _ := base64.StdEncoding.DecodeString(secret)
-	whatIsIt = string(sd)
+	whatIsIt = reverse(string(sd))
 	fmt.Println(whatIsIt)
-
-}
